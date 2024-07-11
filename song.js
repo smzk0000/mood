de967287dac3d58777a9780063d9485699036933
smzk0000 <ms19830917@gmail.com>
2024-07-11 23:38:01 +0900

    Create song.js

---
+++ song.js
@@ -0,0 +1,22 @@
+const songs = {
+    happy: [
+        "spotify:track:4cOdK2wGLETKBW3PvgPWqT",
+        "spotify:track:0WgNp8pxUnZX7D0SrSPAZH",
+        "spotify:track:6DCZcSspjsKoFjzjrWoCdn"
+    ],
+    sad: [
+        "spotify:track:0pqnGHJpmpxLKifKRmU6WP",
+        "spotify:track:7wdwIaXUuzlu1grzWMFRJm",
+        "spotify:track:3AJwUDP919kvQ9QcozQPxg"
+    ],
+    energetic: [
+        "spotify:track:6DCZcSspjsKoFjzjrWoCdn",
+        "spotify:track:3w3y8KPTfNeOKPiqUTakBh",
+        "spotify:track:4cOdK2wGLETKBW3PvgPWqT"
+    ],
+    relaxed: [
+        "spotify:track:3AJwUDP919kvQ9QcozQPxg",
+        "spotify:track:0E4Y1XIbs8GrAT1YqVy6dq",
+        "spotify:track:0pqnGHJpmpxLKifKRmU6WP"
+    ]
+};
