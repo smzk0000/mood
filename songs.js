5a50bba9761b18b4a6a64ae5af1d8aa94737256b
smzk0000 <ms19830917@gmail.com>
2024-07-13 01:46:12 +0900

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,17 +1,48 @@
 const songs = {
     sad: [
         "spotify:track:7n0Kkf7pt5hGrxLtFORF1g",
-        "spotify:track:2MNAUM641g5lKgac4pRUMK"
+        "spotify:track:2MNAUM641g5lKgac4pRUMK",
+        "spotify:track:4llDF3RuBQYj2nkjTCJY4w"
     ],
-    emo: [
+    emotional: [
         "spotify:track:0zO10o0bGFtuENXJgsMKvM"
     ],
     relaxed: [
         "spotify:track:5kLB3w5JPLitJvRpeL14M3",
+        "spotify:track:6Q2p0sJECHZLlef84CRskE",
+        "spotify:track:37HI0imp0t6a0MBS00lPkS",
+        "spotify:track:1KiibjdEIWSrqSMTDcUvsx"
+    ],
+    stroll: [
         "spotify:track:3K7YmyaR13ujAhQV8Wl2EM",
-        "spotify:track:6Q2p0sJECHZLlef84CRskE"
+        "spotify:track:3mOcwPVOL5K5oGw3f8rUuM"
     ],
     angry: [
         "spotify:track:7wHplrQXJbxahRZPlEw7VF"
+    ],
+    thoughtful: [
+        "spotify:track:2hERMN4Fl5CdlEWiWrCLEX",
+        "spotify:track:5gKOsci56ItnPBZO62I4i3",
+        "spotify:track:7emzEVwdmqXmkgAxGD6PNW"
+    ],
+    cheerup: [
+        "spotify:track:1kZvOyo7g6k01Au6DuXY4Y",
+        "spotify:track:1EKk6Xvei9N9scaQcrax2S",
+        "spotify:track:5LaatZmLycE71jtbQNgpM3",
+        "spotify:track:0EIO8ORzhZoYRao0TIjTbv"
+    ],
+    sunrise: [
+        "spotify:track:3cIG78oBauowzEbkBwWADN",
+        "spotify:track:6VRBFg0S36anTof9sY4vyw"
+    ],
+    explosion: [
+        "spotify:track:5Lqq8c2GbJ4L2HPhTs0Yid"
+    ],
+    hard: [
+        "spotify:track:01HmPLtKDSloi6t10gW1El"
+    ],
+    memory: [
+        "spotify:track:6nkFTjaUPW8ogUZCsB9f2w",
+        "spotify:track:7l44k2QJic82TFvZs5Vwff"
     ]
 };
