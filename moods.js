e5e651a5f273547f152f22f1be664c87292e324a
smzk0000 <ms19830917@gmail.com>
2024-07-13 01:46:00 +0900

    Update moods.js

--- moods.js
+++ moods.js
@@ -1,6 +1,13 @@
 const moods = [
     { id: "sad", name: "悲しい", englishName: "sad" },
-    { id: "emo", name: "感動", englishName: "emotional" },
+    { id: "emotional", name: "感動", englishName: "emotional" },
     { id: "relaxed", name: "リラックス", englishName: "relaxed" },
-    { id: "angry", name: "怒り", englishName: "angry" }
+    { id: "stroll", name: "散歩", englishName: "stroll" },
+    { id: "angry", name: "怒り", englishName: "angry" },
+    { id: "thoughtful", name: "沈思", englishName: "thoughtful" },
+    { id: "cheerup", name: "元気出す", englishName: "cheerup" },
+    { id: "sunrise", name: "朝焼け", englishName: "sunrise" },
+    { id: "explosion", name: "爆発", englishName: "explosion" },
+    { id: "hard", name: "困難", englishName: "hard" },
+    { id: "memory", name: "物語", englishName: "memory" }
 ];
