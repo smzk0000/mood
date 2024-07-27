af7e058341eb36953673b72aed9cc59eae69d741
smzk0000 <smzk0000@users.noreply.github.com>
2024-07-27 00:30:13 +0000

    deploy: 13354dcc1e81d589df669182e05dddd14d33455a

--- mood/songs.js
+++ mood/songs.js
@@ -1,48 +1,93 @@
 const songs = {
-    sad: [
+    melancoric: [
         "spotify:track:7n0Kkf7pt5hGrxLtFORF1g",
         "spotify:track:2MNAUM641g5lKgac4pRUMK",
-        "spotify:track:4llDF3RuBQYj2nkjTCJY4w"
+        "spotify:track:4llDF3RuBQYj2nkjTCJY4w",
+        "spotify:track:5gKOsci56ItnPBZO62I4i3",
+        "spotify:track:7emzEVwdmqXmkgAxGD6PNW",
+        "spotify:track:0q1Er6hcbRaiEWj9QwaSb3",
+        "spotify:track:5DCsjSmNQHi1new10CHody",
+        "spotify:track:1I9FowQHF8RmRVw35ZiW2K",
+        "spotify:track:70zCEVxTugFHntItKozubI",
+        "spotify:track:3ofrLJrQjMPuEBQddeAbZn",
+        "spotify:track:1LlEw7wp9AOX0ZkgoOEMxd",
+        "spotify:track:1fMQ3xZqPn5K87R7COXM23",
+        "spotify:track:6Xajr512QEGAZ20bqAWkyT"
     ],
     emotional: [
-        "spotify:track:0zO10o0bGFtuENXJgsMKvM"
+        "spotify:track:0zO10o0bGFtuENXJgsMKvM",
+        "spotify:track:1HfJV18PHF2UQqh4TuySBJ",
+        "spotify:track:0kOBkRmEgUC20do94Zqi9k",
+        "spotify:track:568nXF19QXYPZnQ6XSkuSH",
+        "spotify:track:7h6GcTsaJpCWlvwW1PMUjA"
     ],
     relaxed: [
         "spotify:track:5kLB3w5JPLitJvRpeL14M3",
-        "spotify:track:6Q2p0sJECHZLlef84CRskE",
-        "spotify:track:37HI0imp0t6a0MBS00lPkS",
-        "spotify:track:1KiibjdEIWSrqSMTDcUvsx"
-    ],
-    stroll: [
         "spotify:track:3K7YmyaR13ujAhQV8Wl2EM",
-        "spotify:track:3mOcwPVOL5K5oGw3f8rUuM"
-    ],
-    angry: [
-        "spotify:track:7wHplrQXJbxahRZPlEw7VF"
-    ],
-    thoughtful: [
-        "spotify:track:2hERMN4Fl5CdlEWiWrCLEX",
-        "spotify:track:5gKOsci56ItnPBZO62I4i3",
-        "spotify:track:7emzEVwdmqXmkgAxGD6PNW"
-    ],
-    cheerup: [
+        "spotify:track:6Q2p0sJECHZLlef84CRskE",
         "spotify:track:1kZvOyo7g6k01Au6DuXY4Y",
+        "spotify:track:3mOcwPVOL5K5oGw3f8rUuM",
+        "spotify:track:6VRBFg0S36anTof9sY4vyw",
+        "spotify:track:7M6lSc5QjxDeUFoCDpOXHd",
+        "spotify:track:7Lp6gYzsrAvuv0qPdAJ2yA"
+    ],
+    intense: [
+        "spotify:track:7wHplrQXJbxahRZPlEw7VF",
+        "spotify:track:01HmPLtKDSloi6t10gW1El",
+        "spotify:track:255ET1WLnkSYN0uzEOuaRq",
+        "spotify:track:6dLkABcI4OB65ObwZT7Lao",
+        "spotify:track:0WCk7NesqMEkzMloCvChxB",
+        "spotify:track:5xS2rxwtXD4vS2tTU9kzfQ",
+        "spotify:track:05aYSYeteDE0bXJWb7sYjr",
+        "spotify:track:5SbbA2y1phJ4ezeVihR4re",
+        "spotify:track:07nYZZQT49i7yAIs3k3tOe",
+        "spotify:track:1EqPpTJxzKSSWjx8nAQtmX",
+        "spotify:track:2Y9pSmDn8UoqSkg88IDaW2",
+        "spotify:track:0glEvaWrhIP0boU56o5Q9U",
+        "spotify:track:3cld8uMQZok9Z8TCw041zF"
+    ],
+    drone: [
+        "spotify:track:2hERMN4Fl5CdlEWiWrCLEX"
+    ],
+    cinematic: [
+        "spotify:track:37HI0imp0t6a0MBS00lPkS",
         "spotify:track:1EKk6Xvei9N9scaQcrax2S",
         "spotify:track:5LaatZmLycE71jtbQNgpM3",
-        "spotify:track:0EIO8ORzhZoYRao0TIjTbv"
-    ],
-    sunrise: [
-        "spotify:track:3cIG78oBauowzEbkBwWADN",
-        "spotify:track:6VRBFg0S36anTof9sY4vyw"
-    ],
-    explosion: [
-        "spotify:track:5Lqq8c2GbJ4L2HPhTs0Yid"
-    ],
-    hard: [
-        "spotify:track:01HmPLtKDSloi6t10gW1El"
-    ],
-    memory: [
+        "spotify:track:5Lqq8c2GbJ4L2HPhTs0Yid",
         "spotify:track:6nkFTjaUPW8ogUZCsB9f2w",
-        "spotify:track:7l44k2QJic82TFvZs5Vwff"
+        "spotify:track:7l44k2QJic82TFvZs5Vwff",
+        "spotify:track:320l1iWFAa1slHDowwI2vB",
+        "spotify:track:3yp0flNlzieUXkI1epZYUI",
+        "spotify:track:4Yz19EAgGhlVTA8Q0XodnF",
+        "spotify:track:4UZ6AijaMzwsoeKQ4BJUk3",
+        "spotify:track:1jWfJBkfoemq9TjC0jpbU7",
+        "spotify:track:2h71cBOu9dA49iglmD6ayb",
+        "spotify:track:78SsvsCtQlUOxwJ5pyyszw",
+        "spotify:track:3HBcQ5GenzAlBhCIgDGL3x",
+        "spotify:track:7JycE22P5xi1WwNtuGUjOv",
+        "spotify:track:0r2FW7dxoekEDiylEvcecO",
+        "spotify:track:1scLI3CLQztRJF0Xk5VEbl",
+        "spotify:track:3Qq05M2i4m89LVeQ8ZK2ek",
+        "spotify:track:4i3877NjqgIZlzyMcxh8TZ"
+    ],
+    meditation: [
+        "spotify:track:3cIG78oBauowzEbkBwWADN",
+        "spotify:track:4CAqn3G9Nzq4GifRv7gW2D",
+        "spotify:track:5HwSgmQZYM0laGsogMiMI7",
+        "spotify:track:1ocxp1ncDQF8UxpYjVQ8cX",
+        "spotify:track:4q3c1siZTggTRiT1PeRu2z",
+        "spotify:track:3LHINSuy4dL5Lgvg0q8M2k"
+    ],
+    soundscape: [
+        "spotify:track:0DgiShjYS9dR537WGITGrP",
+        "spotify:track:282xkc8wWvstwbegsX9aCh",
+        "spotify:track:213ABDaJfIte2YHACSDXrq",
+        "spotify:track:3lG9eYlhnipGfvAR4ZkBk7",
+        "spotify:track:2RP7q7qBxg35vgvfCwlstD",
+        "spotify:track:4xh0pq6fa768xtuheAoP0p"
+    ],
+    math: [
+        "spotify:track:7r14vA3YsfA2gHO5FyVYrP",
+        "spotify:track:2RJuCZXtZYubzTW78kmaBx"
     ]
 };
