218daab1274c1772cc8a4c7adce8f2232ab3ff40
smzk0000 <smzk0000@users.noreply.github.com>
2024-07-27 00:30:02 +0000

    deploy: ee45e7e38097159146b0324a320490a775bf9c5d

--- mood/moods.js
+++ mood/moods.js
@@ -1,13 +1,11 @@
 const moods = [
-    { id: "sad", name: "悲しい", englishName: "sad" },
-    { id: "emotional", name: "感動", englishName: "emotional" },
-    { id: "relaxed", name: "リラックス", englishName: "relaxed" },
-    { id: "stroll", name: "散歩", englishName: "stroll" },
-    { id: "angry", name: "怒り", englishName: "angry" },
-    { id: "thoughtful", name: "沈思", englishName: "thoughtful" },
-    { id: "cheerup", name: "元気出す", englishName: "cheerup" },
-    { id: "sunrise", name: "朝焼け", englishName: "sunrise" },
-    { id: "explosion", name: "爆発", englishName: "explosion" },
-    { id: "hard", name: "困難", englishName: "hard" },
-    { id: "memory", name: "物語", englishName: "memory" }
+    { id: "melancoric", englishName: "Melancoric" },
+    { id: "emotional", englishName: "Emotional" },
+    { id: "relaxed", englishName: "Relaxed" },
+    { id: "intense", englishName: "Intense" },
+    { id: "drone", englishName: "Drone" },
+    { id: "cinematic", englishName: "Cinematic" },
+    { id: "meditation", englishName: "Meditation" },
+    { id: "soundscape", englishName: "Soundscape" },
+    { id: "math", englishName: "Math" }
 ];
